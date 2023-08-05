f83b105b431d496bee0ade143e8ffe234062c261
Nataliya Pisareva <natapis2009@mail.ru>
2023-08-05 08:58:55 +0300

    Lion сделала feline private

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,14 +5,13 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    Feline feline;
+    private final Feline feline;
 
     public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
-            this.feline = feline;
             hasMane = true;
         } else if ("Самка".equals(sex)) {
-            this.feline = feline;
             hasMane = false;
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самец или самка");
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,11 +1,13 @@
 package com.example;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.List;
-
-
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 
     @Mock
@@ -14,7 +16,7 @@ public class LionTest {
     public void getKittensTest(){
         try {
             Lion lion = new Lion("Самка", feline);
-            Mockito.when(feline.getKittens(Mockito.anyInt())).thenReturn(1);
+            Mockito.when(feline.getKittens()).thenReturn(1);
             int actualResult = lion.getKittens();
             int expectedResult = 1;
             Assert.assertEquals(expectedResult,actualResult);
@@ -26,8 +28,8 @@ public class LionTest {
         try{
             Lion lion = new Lion("Самец", feline);
             Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-            List<String> expectedResult = lion.getFood();
-            List<String> actualResult = List.of("Животные", "Птицы", "Рыба");
+            List<String> actualResult = lion.getFood();
+            List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
             Assert.assertEquals(expectedResult,actualResult);
         } catch (Exception e){
             System.out.println(e);
